105b845033b91e4ceff010dc07f83c3c4923a341
TheSlowly <theslowlyprivate@gmail.com>
2025-03-07 12:02:45 -0500

    Remove current LoE-Launcher

--- LoE-Launcher_Old/Properties/AssemblyInfo.cs
+++ LoE-Launcher_Old/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LoE-Launcher")]
+[assembly: AssemblyTitle("LoE-Launcher_Old")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LoE-Launcher")]
+[assembly: AssemblyProduct("LoE-Launcher_Old")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
