a9802d9ca924c38f15ab51442b16222174a58b32
TheSlowly <theslowlyprivate@gmail.com>
2025-07-12 04:28:40 -0400

    Fix possible zsync root uri issues

--- LoE-Launcher/Core/Settings.cs
+++ LoE-Launcher/Core/Settings.cs
@@ -3,7 +3,7 @@
 public class Settings
 {
     public bool IgnoreSSLCertificates { get; set; } = false;
-
+    
     public string ZsyncLocation { get; set; } = "http://patches.legendsofequestria.com/zsync/{version}/";
         
     public string Stream { get; set; } = "http://patches.legendsofequestria.com/zsync/versions3.json";
@@ -11,5 +11,8 @@ public class Settings
     public string LauncherVersionUrl { get; set; } = "http://patches.legendsofequestria.com/launcher-version.json";
 
     internal string FormatZsyncLocation(string version)
-        => ZsyncLocation.Replace("{version}", version);
+    {
+        var formatted = ZsyncLocation.Replace("{version}", version);
+        return formatted.EndsWith('/') ? formatted : formatted + '/';
+    }
 }
