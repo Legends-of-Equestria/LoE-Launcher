bfe726b422e1ec68d2604435added47b60e11876
TheSlowly <theslowlyprivate@gmail.com>
2025-10-23 16:21:58 -0400

    Remove legacy code and projects

--- LoE-Launcher_Old/Core/IRelativeDirectoryPath.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LoE_Launcher.Core
-{
-    public interface IRelativeDirectoryPath
-    {
-        IAbsoluteDirectoryPath GetAbsolutePathFrom(IAbsoluteDirectoryPath launcherPath);
-        string Path { get; }
-        IRelativeFilePath GetChildFileWithName(string fileName);
-    }
-}
